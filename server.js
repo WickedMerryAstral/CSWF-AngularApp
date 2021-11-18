72349d5f3ecbe03e198e354736bce24dea5ab841
Lynn <astralvissers@gmail.com>
2021-11-18 15:16:48 +0100

    Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/StoryManager'));
+app.use(express.static(__dirname + '/StoryManager'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/StoryManager/index.html'));
+  res.sendFile(path.join(__dirname + '/StoryManager'));
 });
 
 // Start the app by listening on the default Heroku port
