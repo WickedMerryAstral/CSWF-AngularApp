f2402742c104a0b479067c80872c824d6d47a28a
Lynn <astralvissers@gmail.com>
2021-11-18 17:13:08 +0100

    Changed key again

--- StoryManager/server.js
+++ StoryManager/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/story-manager'));
+app.use(express.static(__dirname + '/story-manager'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    '/dist/story-manager/index.html'));
+    'build', 'index.html'));
 });
 app.listen(process.env.PORT || 8080);
