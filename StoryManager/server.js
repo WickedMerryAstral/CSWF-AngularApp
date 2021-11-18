9d56526c46edf0ff25551bdd49d250b43a51f1df
Lynn <astralvissers@gmail.com>
2021-11-18 17:36:49 +0100

    Production build

--- StoryManager/server.js
+++ StoryManager/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/StoryManager/src'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    '/src/index.html'));
+    '/dist/StoryManager/index.html'));
 });
 app.listen(process.env.PORT || 8080);
