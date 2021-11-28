84602f43f5630b8fe37defa9b12105549e8d4e23
Lynn <astralvissers@gmail.com>
2021-11-28 18:18:56 +0100

    Tests

--- StoryManager/src/app/story.service.spec.ts
+++ StoryManager/src/app/story.service.spec.ts
@@ -1,19 +1,56 @@
 import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { async, getTestBed, inject, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { Story } from '../model/story';
+import { User } from '../model/user';
 
 import { StoryService } from './story.service';
+import { UserService } from './user.service';
 
 describe('StoryService', () => {
-  let service: StoryService;
+  let injector: TestBed;
+  let storyService: StoryService;
+  let userService: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler]
+      providers: [HttpClient, HttpHandler, HttpClientTestingModule]
     });
-    service = TestBed.inject(StoryService);
+    injector = getTestBed();
+    storyService = injector.get(StoryService);
+  });
+
+  afterEach(() => {
+    injector.resetTestingModule();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(storyService).toBeTruthy();
   });
+
+  it('should return observable stories', () => {
+    let temp: Story[];
+
+    inject([StoryService], async (storyService: StoryService) => {
+      storyService.getStories()
+        .subscribe(result => {
+          temp = result;
+        });
+    });
+
+    expect(temp).not.toBeNull();
+  });
+
+  it('should only add stories to valid users',
+    inject([StoryService], async (storyService: StoryService) => {
+      var story = new Story();
+      story.author = new User();
+      story.author._id = "fakeid";
+      story.title = "testle";
+      story.description = "testcription";
+
+      const result = await storyService.postStory(story);
+      expect(result).not.toBeNull()
+    }));
 });
